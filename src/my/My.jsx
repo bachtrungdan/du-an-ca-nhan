1dcefa24e172c0ed7db77895c567c25ba92a52fc
bachdan <bachtrungdan@gmail.com>
2022-11-20 15:57:21 +0700

    add

--- src/my/My.jsx
+++ src/my/My.jsx
@@ -3,10 +3,8 @@ import '../my/my.css'
 
 function My() {
     return (
-        <div>  bah trung dan 
-            {/* <img style={'--i:1;'} src="http://www.nicdarkthemes.com/themes/travel/wp/demo/love-travel/wp-content/uploads/sites/3/2018/04/destination-9.jpg" alt="" />
-            <img src="https://vcdn1-dulich.vnecdn.net/2020/09/04/1-Meo-chup-anh-dep-khi-di-bien-9310-1599219010.jpg?w=0&h=0&q=100&dpr=2&fit=crop&s=j8THd4HE31ZHWTQhSZx5tQ" alt="" /> */}
-           <div class="box">
+        <div>  bah trung dan
+            <div class="box">
                 <span >
                     <img src="/accset/img/181095-gia-dinh.jpg" />
                 </span>
@@ -26,7 +24,7 @@ function My() {
                 </span>
                 <span>
                     <img src="http://www.nicdarkthemes.com/themes/travel/wp/demo/love-travel/wp-content/uploads/sites/3/2018/03/destination-1-1-720x720.jpg" />
-                    
+
                 </span>
                 <span>
                     <img src="http://www.nicdarkthemes.com/themes/travel/wp/demo/love-travel/wp-content/uploads/sites/3/2018/03/destination-3.jpg" />
